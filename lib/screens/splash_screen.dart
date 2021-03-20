2cded248f2f6e3e4bb5caeb069790dc25c0b7a89
fauzanakmalh1 <fauzanakmalhariz1@gmail.com>
2021-03-20 13:50:53 +0700

    update splash screen 1 & 2

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,23 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:parking_u/screens/home_screen.dart';
-import 'package:splashscreen/splashscreen.dart';
+import 'dart:async';
 
-class SplashScreenPage extends StatelessWidget {
+class SplashScreenPage extends StatefulWidget {
+  @override
+  _SplashScreenPageState createState() => _SplashScreenPageState();
+}
+
+class _SplashScreenPageState extends State<SplashScreenPage> {
+  @override
+  void initState() {
+    super.initState();
+    startSplashScreen();
+  }
+
+  startSplashScreen() async {
+    var duration = const Duration(seconds: 5);
+    return Timer(duration, () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_) {
+          return HomeScreen();
+        }),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new SplashScreen(
-      seconds: 3,
-      navigateAfterSeconds: new HomeScreen(),
-      title: new Text(
-        'Parking.u',
-        style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+    return Scaffold(
+      backgroundColor: Color(0XFFFFFFFF),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 240),
+            Image.asset(
+              "assets/images/logo-parking-u.png",
+              width: 60.0,
+              height: 60.0,
+            ),
+            Image.asset(
+              "assets/images/logo-parking-u-text.png",
+              width: 200.0,
+              height: 90.0,
+            ),
+            Text(
+              'Cari parkir tidak pernah semudah ini',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Image.asset(
+                "assets/images/wave.png",
+                width: 370.0,
+                height: 345.0,
+              ),
+            ),
+          ],
+        ),
       ),
-      image: new Image.asset('assets/images/logo-parking-u.png'),
-      backgroundColor: Colors.white,
-      styleTextUnderTheLoader: new TextStyle(),
-      photoSize: 35.0,
-      loaderColor: Colors.blue,
-      loadingText: Text('Cari parkir tidak pernah semudah ini'),
     );
   }
 }
